6b689535343626919dbb7c59827474787fba4b45
Harin Ramji <harrama6@student.wintec.ac.nz>
2024-05-02 13:19:09 +1200

    changes to stats...1

--- task4_1/Pages/Statistics.xaml.cs
+++ task4_1/Pages/Statistics.xaml.cs
@@ -2,9 +2,34 @@ namespace task4_1.Pages;
 
 public partial class Statistics : ContentPage
 {
-	public Statistics(MainViewModel vm)
+    public List<Animal> Animals { get; set; }
+    public double GovernmentTaxFor30Days { get; set; }
+    public double FarmDailyProfit { get; set; }
+    public Statistics(MainViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
 	}
+
+    private void CalulateStats(object sender, EventArgs e)
+    {
+        double governmentTaxRate = 0.02; // Government tax rate per kg per day
+        double totalWeight = Animals.Sum(animal => animal.Weight);
+        GovernmentTaxFor30Days = governmentTaxRate * totalWeight * 30;
+
+        // Calculate farm daily profit
+        double cowMilkPrice = 9.4; // $ per kg
+        double sheepWoolPrice = 6.2; // $ per kg
+        double totalIncome = Animals.Sum(animal =>
+        {
+            if (animal is Cow cow)
+                return cow.Milk * cowMilkPrice;
+            else if (animal is Sheep sheep)
+                return sheep.Wool * sheepWoolPrice;
+            else
+                return 0;
+        });
+        double totalCost = Animals.Sum(animal => animal.Cost);
+        FarmDailyProfit = totalIncome - totalCost - GovernmentTaxFor30Days;
+    }
 }
